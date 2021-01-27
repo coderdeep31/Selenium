03a5a1a076c3f6a94a9717167bf6ac91488df612
Deepmoy Banerjee <75161113+coderdeep31@users.noreply.github.com>
2021-01-27 10:06:03 +0530

    Update Flipkartauto.py

--- Flipkart Checkout/Flipkartauto.py
+++ Flipkart Checkout/Flipkartauto.py
@@ -1,56 +1,33 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import time
-
-
-class FlipKart():
-    def test(self):
-        baseUrl = "https://flipkart.com/"
-        driver = webdriver.Chrome()
-        driver.maximize_window()
-        driver.get(baseUrl)
-        driver.implicitly_wait(3)
-
-        # Enter Credentials
-        userName = usr_name = driver.find_element_by_css_selector("input[Class='_2IX_2- VJZDxU']")
-        userName.send_keys("Provide your Id or Phone Number")
-
-        password = driver.find_element_by_css_selector("input[Class='_2IX_2- _3mctLh VJZDxU']")
-        password.send_keys("Provide Your Password")
-
-        # Click on Login button
-        login = driver.find_element_by_css_selector("button[Class='_2KpZ6l _2HKlqd _3AWRsL']").click()
-        print("Login Successful !!!")
-        time.sleep(3)
-        # Find parent Handle
-        parentHandle = driver.current_window_handle
-        print("Parent Handle is : " + parentHandle)
-
-        # Search for available Selenium books
-        searchName = driver.find_element(By.XPATH, "//input[@title='Search for products, brands and more']")
-        searchName.send_keys("Selenium Book")
-        clickSearch = driver.find_element(By.XPATH, "//button[@type='submit']")
-        clickSearch.click()
-
-        # Select First Book
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get("http://flipkart.com")
+usr_name = driver.find_element_by_css_selector("input[Class='_2IX_2- VJZDxU']")
+usr_name.send_keys("Provide Your Flipkart Login ID/Phone No.")
+pswd = driver.find_element_by_css_selector("input[Class='_2IX_2- _3mctLh VJZDxU']")
+pswd.send_keys("Provide your Flipkart password")
+time.sleep(2)
+driver.find_element_by_css_selector("button[Class='_2KpZ6l _2HKlqd _3AWRsL']").click()
+time.sleep(2)
+driver.find_element_by_css_selector("input[class='_3704LK']").send_keys("Apple iPad" + Keys.ENTER)
+parenthandle = driver.current_window_handle
+print(parenthandle)
+time.sleep(3)
+driver.find_element_by_xpath("(//a[contains(text(),'Apple iPad')])[1]").click()
+time.sleep(5)
+handles = driver.window_handles
+for handle in handles:
+    print("handle:" + handle)
+    if handle not in parenthandle:
+        driver.switch_to.window(handle)
+        print(driver.current_window_handle)
+        driver.find_element_by_css_selector("button[class='_2KpZ6l _2U9uOA ihZ75k _3AWRsL']").click()
+        time.sleep(15)
+        driver.find_element_by_css_selector("button[class='_2KpZ6l RLM7ES _3AWRsL']").click()
+        time.sleep(4)
+        driver.find_element_by_css_selector("button[class='_2KpZ6l _1seccl _3AWRsL']").click()
         time.sleep(5)
-        firstBook = driver.find_element(By.XPATH, "(//a[contains(text(), 'Selenium')])[1]")
-        firstBook.click()
-
-        # Find all handles, there should be two handles after clicking Book Link
-        handles = driver.window_handles
-        print("Handless are : ", handles)
-
-        # Switch to Tab
-        for handle in handles:
-            print("Handle is : " + handle)
-            if handle not in parentHandle:
-                driver.switch_to.window(handle)
-                print("Switched to Handle : " + handle)
-                time.sleep(3)
-                addToCart = driver.find_element(By.XPATH, "//button[@class='_2KpZ6l _2U9uOA ihZ75k _3AWRsL']")
-                addToCart.click()
-
-
-f = FlipKart()
-f.test()
+        driver.close()
+driver.quit()
